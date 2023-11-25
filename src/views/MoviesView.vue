d73ff8533c083cff63cf39a0aa7e29d7f23dd5a6
Tatiana Sanchez <tatianasanchez03@gmail.com>
2023-11-25 12:48:29 +0100

    Add vue3-carousel to show the movies in the view

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -3,3 +3,54 @@
     <h1>This is the movies view</h1>
   </div>
 </template>
+ <template>
+  <carousel :items-to-show="5">
+    <slide v-for="movie in latestReleases" :key="movie">
+      {{ movie }}
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+</template>
+
+<script>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+export default {
+  name: 'App',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+  data() {
+    return {
+      latestReleases: ['movie1', 'movie2'],
+      userRatedMovies: [],
+      allFilms: [],
+    };
+  },
+  
+  methods: {
+    fetchLatestReleases() {
+      
+    },
+    fetchUserRatedMovies() {
+      
+    },
+    fetchAllFilms() {
+      
+    },
+  },
+  mounted() {
+    this.fetchLatestReleases();
+    this.fetchUserRatedMovies();
+    this.fetchAllFilms();
+  },
+}
+</script>
